e62003b35ef565ab2b60348b802382e13839d1d5
an4ek <annalebedeva0911@gmail.com>
2024-11-26 23:11:42 +0700

    Initial commit

---
+++ src/main/java/org/example/Odd.java
@@ -0,0 +1,18 @@
+package org.example;
+
+//Реализуем интерфейс Runnable
+public class Odd implements Runnable {
+
+    public void run() {
+        for (int i = 0; i <= 10; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
